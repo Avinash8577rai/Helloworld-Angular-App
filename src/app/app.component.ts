49ed69664a903d9165c6f8d17d7a93dce07b61df
Avinash8577rai <avinashkumarrai8577@gmail.com>
2025-03-05 15:50:59 +0530

    [Avinash] Added One-Way Data Binding using Interpolation from bridge labz

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,4 +9,5 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'Helloworld-Angular-App';
+  message: string = 'Hello from BridgeLabz using One Way Data Bindings';
 }
