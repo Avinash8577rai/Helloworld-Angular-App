1dfdda711130464e03825e332fdd9cdc9baa2de6
Avinash8577rai <avinashkumarrai8577@gmail.com>
2025-03-05 16:17:32 +0530

    [Avinash] Added Property Binding for BridgeLabz Logo in my project that is Helloworld Angular App

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,4 +10,5 @@ import { RouterOutlet } from '@angular/router';
 export class AppComponent {
   title = 'Helloworld-Angular-App';
   message: string = 'Hello from BridgeLabz using One Way Data Bindings';
+  logoUrl: string = 'assets/BL_logo_square_jpg.jpg';
 }
